7f592e9bc5c6ff1be70e7398f025ee9042ba22dd
ramishawasim <wasimramisha@gmail.com>
2026-03-22 18:21:16 -0400

    update main

--- public/js/main.js
+++ public/js/main.js
@@ -90,7 +90,7 @@ function onLocationFound(e) {
     var radius = e.accuracy;
 
     L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        .bindPopup("This is where you are").openPopup();
 
     L.circle(e.latlng, radius).addTo(map);
 }
